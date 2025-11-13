e636828aa88342fda51e4644e76368f8c4f45888
Keshav Worathur <keshavworathur@gmail.com>
2025-11-13 16:24:41 -0500

    fix: merge conflicts in signup form

--- src/components/SignUpForm.tsx
+++ src/components/SignUpForm.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import FormInput from "./FormInput";
 
 
@@ -5,7 +6,7 @@ import FormInput from "./FormInput";
 const LoginForm = () => {
     return <div className='w-[700px] h-[682px] flex flex-col'>
 
-        <h1 className="text-center text-6xl">Create Account</h1>
+        <h1 className="text-center font-bold text-6xl">Create Account</h1>
         <div className='mt-4 flex-1 flex flex-col justify-around'>
             <FormInput type={'text'} label={'Full Name'} placeholder="Full Name" />
             <FormInput type={'email'} label={'Email'} placeholder="Email" />
@@ -14,6 +15,7 @@ const LoginForm = () => {
             <FormInput type={'checkbox'} label={'Admin Access'} />
         </div>
         <button className='mt-4 p-4 rounded-xl border-2 bg-red-700 text-white text-4xl font-bold'>Sign up</button>
+        <p className='pt-4 text-center'>Already have an account? <Link href="/signin" className="font-bold">Sign In</Link></p>
     </div>
 }
  
