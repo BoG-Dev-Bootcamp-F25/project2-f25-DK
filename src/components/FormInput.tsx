a45bf7b4e04078ee0a771f83dcc0c60d60fa3cd8
Keshav Worathur <keshavworathur@gmail.com>
2025-11-13 14:49:57 -0500

    feat(ui): create signup ui

--- src/components/FormInput.tsx
+++ src/components/FormInput.tsx
@@ -1,13 +1,42 @@
-
 type FormInputProps = {
     label: string;
-    type: 'text' | 'email' | 'password'
-}
+    type: 'text' | 'email' | 'password' | 'checkbox';
+    placeholder?: string;
+};
 
-const FormInput = ({label, type} : FormInputProps) => {
-    return <div className='mt-2'>
-        <label></label><input className='w-100' placeholder="some input"></input>
-    </div>
-}
- 
+const FormInput = ({ label, type, placeholder }: FormInputProps) => {
+    const labelId = `form-${label.replaceAll(' ', '-')}`;
+    return (
+        <div className="m-4 flex flex-col items-start">
+            {type != 'checkbox' ? (
+                <>
+                    <label htmlFor={labelId} className="pb-4 font-medium">
+                        {label}
+                    </label>
+                    <input
+                        id={labelId}
+                        type={type}
+                        className="flex-1 w-full border-b-4 border-red-500"
+                        placeholder={placeholder}
+                    ></input>
+                </>
+            ) : (
+                <>
+                    <div className="flex items-center gap-2">
+                        <input
+                            id={labelId}
+                            type={type}
+                            className="flex-1 w-full border-b-4 border-red-500 text-center"
+                            placeholder={placeholder}
+                        ></input>
+                        <label htmlFor={labelId} className="font-medium">
+                            {label}
+                        </label>
+                    </div>
+                </>
+            )}
+        </div>
+    );
+};
+
 export default FormInput;
--- src/components/SignUpForm.tsx
+++ src/components/SignUpForm.tsx
@@ -5,9 +5,15 @@ import FormInput from "./FormInput";
 const LoginForm = () => {
     return <div className='w-[700px] h-[682px] flex flex-col'>
 
-        <h1>Create Account</h1>
-        <FormInput type={'text'} label={'Full Name'} />
-        <button>Sign up</button>
+        <h1 className="text-center text-6xl">Create Account</h1>
+        <div className='mt-4 flex-1 flex flex-col justify-around'>
+            <FormInput type={'text'} label={'Full Name'} placeholder="Full Name" />
+            <FormInput type={'email'} label={'Email'} placeholder="Email" />
+            <FormInput type={'password'} label={'Password'} placeholder="Password" />
+            <FormInput type={'password'} label={'Confirm Password'} placeholder="Confirm Password" />
+            <FormInput type={'checkbox'} label={'Admin Access'} />
+        </div>
+        <button className='mt-4 p-4 rounded-xl border-2 bg-red-700 text-white text-4xl font-bold'>Sign up</button>
     </div>
 }
  
