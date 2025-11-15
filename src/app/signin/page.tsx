d3ad63c8d98b4ba323ebd7732c5a40ad95720ddc
Keshav Worathur <keshavworathur@gmail.com>
2025-11-14 22:51:16 -0500

    feat(ui): add signin route

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,10 +1,10 @@
-import SignUpForm from "@/components/SignUpForm";
+import LoginForm from "@/components/LoginForm";
 
-export default function SignUpPage() {
+export default function SignInPage() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full flex-col justify-center items-center py-32 px-16 bg-white dark:bg-black">
-        <SignUpForm />
+        <LoginForm />
       </main>
     </div>
   );
