74ed4b2fc89aef53ee8cb8f211f017a3cd8c7b46
Keshav Worathur <keshavworathur@gmail.com>
2025-11-14 22:50:59 -0500

    feat(ui): Implement login and signup components

--- src/components/FormInput.tsx
+++ src/components/FormInput.tsx
@@ -4,19 +4,20 @@ type FormInputProps = {
     placeholder?: string;
 };
 
-const FormInput = ({ label, type, placeholder }: FormInputProps) => {
+const FormInput = ({ label, type, placeholder, ...props }: FormInputProps) => {
     const labelId = `form-${label.replaceAll(' ', '-')}`;
     return (
         <div className="m-4 flex flex-col items-start">
             {type != 'checkbox' ? (
                 <>
-                    <label htmlFor={labelId} className="pb-4 font-medium">
+                    <label htmlFor={labelId} className="pb-4 font-medium hidden">
                         {label}
                     </label>
                     <input
+                        {...props}
                         id={labelId}
                         type={type}
-                        className="flex-1 w-full border-b-4 border-red-500"
+                        className="flex-1 pt-2 text-2xl w-full border-0 border-b-4 border-red-500"
                         placeholder={placeholder}
                     ></input>
                 </>
@@ -26,10 +27,10 @@ const FormInput = ({ label, type, placeholder }: FormInputProps) => {
                         <input
                             id={labelId}
                             type={type}
-                            className="flex-1 w-full border-b-4 border-red-500 text-center"
+                            className="flex-1 pt-2 appearance-none w-6 h-6 border-2 border-red-400 rounded-sm bg-white checked:bg-red-500 checked:border-red-500 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
                             placeholder={placeholder}
                         ></input>
-                        <label htmlFor={labelId} className="font-medium">
+                        <label htmlFor={labelId} className="text-2xl font-medium">
                             {label}
                         </label>
                     </div>
