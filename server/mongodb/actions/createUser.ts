0a426c0dd277840cc8f13e4b9e521e5692b81cd5
Keshav Worathur <keshavworathur@gmail.com>
2025-11-16 10:55:23 -0500

    feat(models): define User model

---
+++ server/mongodb/actions/createUser.ts
@@ -0,0 +1,28 @@
+import { InferSchemaType } from 'mongoose';
+import connectDB from '..';
+import { User } from '../models/User';
+
+interface UserData {
+    name: string;
+    age?: number;
+}
+
+interface CreateUserResponse{
+    data: UserData & {id_: string}
+}
+
+const createUser = async (userData: UserData): Promise<CreateUserResponse | null> => {
+    try{
+        const newUser = new User(userData);
+        await connectDB();
+        await newUser.save();
+        //@ts-ignore
+        const responseData: CreateUserResponse = {data: {name: newUser.name, age: newUser.age, id_: newUser.id}};
+        return responseData
+    } catch (err) {
+        console.error(`[ERROR]: Error encountered while creating user: ${err}`);
+        return null;
+    }
+};
+
+export default createUser;
---
+++ server/mongodb/models/User.ts
@@ -0,0 +1,22 @@
+import mongoose, { InferSchemaType, Model, Schema, model, models } from "mongoose";
+
+const userSchema = new Schema({
+	fullName: {
+		type: String,
+		required: true,
+	},
+    email: {
+        type: String,
+        required: true,
+        
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    admin : {
+        type: Boolean,
+    }
+});
+
+export const User: Model<InferSchemaType<typeof userSchema>> = models.User ?? model('User', userSchema);
