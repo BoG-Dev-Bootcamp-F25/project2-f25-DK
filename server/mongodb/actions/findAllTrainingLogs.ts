07e8c04c49d3fa6bab4fa0352cb5330b9bea96fa
Keshav Worathur <keshavworathur@gmail.com>
2025-11-20 10:52:35 -0500

    fix(api): sort training logs by date ascending

--- server/mongodb/actions/findAllTrainingLogs.ts
+++ server/mongodb/actions/findAllTrainingLogs.ts
@@ -7,7 +7,6 @@ const findAllTrainingLogs = async (): Promise<TrainingLogDocument[]> => {
         const logs = (await TrainingLog.find({}).exec()).sort((a, b) =>
             a.date < b.date ? -1 : b.date > a.date ? 1 : 0
         );
-        console.log('Sorted training logs ', logs);
         return logs;
     } catch (err) {
         console.error(`[ERROR]: Error encountered while creating user: ${err}`);
