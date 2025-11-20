5bd8ed7e31ccc5b001f8c5d51b427b0930ef82df
Keshav Worathur <keshavworathur@gmail.com>
2025-11-19 23:20:02 -0500

    fix: inconsistent padding on form headers

--- src/app/dashboard/animals/new/page.tsx
+++ src/app/dashboard/animals/new/page.tsx
@@ -1,15 +1,15 @@
-import EditAnimalForm from "@/components/EditAnimalForm";
+import EditAnimalForm from '@/components/EditAnimalForm';
 
 export default function SignInPage() {
     return (
-            <main className="min-h-full w-full bg-white dark:bg-black">
-                <h1 className="mt-8 pl-8 pb-2 text-left text-4xl text-neutral-500 ">
-                    New Animal
-                </h1>
-                <hr />
-                <div className="my-4 flex flex-col justify-center items-center">
-                    <EditAnimalForm />{' '}
-                </div>
-            </main>
+        <main className="min-h-full w-full bg-white dark:bg-black">
+            <h1 className="pl-4 pt-8 text-left text-2xl font-bold text-neutral-600">
+                New Animal
+            </h1>
+            <hr />
+            <div className="my-4 flex flex-col justify-center items-center">
+                <EditAnimalForm />{' '}
+            </div>
+        </main>
     );
 }
--- src/app/dashboard/training-logs/edit/[id]/page.tsx
+++ src/app/dashboard/training-logs/edit/[id]/page.tsx
@@ -3,12 +3,10 @@ import EditTrainingLogForm from '@/components/EditTrainingLogForm';
 export default function SignInPage() {
     return (
         <main className="h-full w-full bg-white dark:bg-black flex flex-col min-h-0">
-            <div>
-                <h1 className="p-8 pl-8 pb-2 text-left text-4xl text-neutral-500 ">
-                    Training logs
-                </h1>
-                <hr />
-            </div>
+            <h1 className="pl-4 pt-8 text-left text-2xl font-bold text-neutral-600">
+                Training logs
+            </h1>
+            <hr />
             <div className="min-h-0 overflow-y-auto flex-1 flex justify-center items-center">
                 <EditTrainingLogForm />{' '}
             </div>
