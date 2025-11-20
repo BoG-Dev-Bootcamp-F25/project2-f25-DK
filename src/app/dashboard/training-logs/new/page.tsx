2e321bcfa9d3b717b64c1ae7251c319d93d938d0
Keshav Worathur <keshavworathur@gmail.com>
2025-11-20 13:12:59 -0500

    feat(ui): make form overflow on smaller viewports

--- src/app/dashboard/training-logs/new/page.tsx
+++ src/app/dashboard/training-logs/new/page.tsx
@@ -2,7 +2,7 @@ import EditTrainingLogForm from '@/components/EditTrainingLogForm';
 
 export default function SignInPage() {
     return (
-        <main className="h-full min-h-0 overflow-hidden  w-full  dark:bg-black">
+        <main className="h-full min-h-0 overflow-hidden flex flex-col w-full  dark:bg-black">
             <h1 className="pl-4 pt-8 text-left text-2xl font-bold text-neutral-600">
                 New Training Log
             </h1>
