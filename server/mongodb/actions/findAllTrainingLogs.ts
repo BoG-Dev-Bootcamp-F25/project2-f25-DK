1a5982c9aecab3653e9a69fbae72cd5f1efd9c9f
Keshav Worathur <keshavworathur@gmail.com>
2025-11-19 20:42:05 -0500

    feat(ui): replace static data with dynamic data from db

--- server/mongodb/actions/findAllTrainingLogs.ts
+++ server/mongodb/actions/findAllTrainingLogs.ts
@@ -1,15 +1,14 @@
 import connectDB from '..';
 import { TrainingLog, TrainingLogDocument } from '../models/TrainingLog';
 
-
-const findAllTrainingLogs = async (): Promise<TrainingLogDocument[] | null> => {
+const findAllTrainingLogs = async (): Promise<TrainingLogDocument[]> => {
     try {
         await connectDB();
         const logs = await TrainingLog.find({}).exec();
         return logs;
     } catch (err) {
         console.error(`[ERROR]: Error encountered while creating user: ${err}`);
-        return null;
+        return [];
     }
 };
 
--- src/app/dashboard/training-logs/page.tsx
+++ src/app/dashboard/training-logs/page.tsx
@@ -1,4 +1,4 @@
-"use client";
+'use client';
 
 import LoginForm from '@/components/LoginForm';
 import TrainingLogCard, { mockData } from '@/components/TrainingLogCard';
@@ -6,41 +6,73 @@ import Link from 'next/link';
 import Image from 'next/image';
 import { useEffect, useState } from 'react';
 import { useSession } from 'next-auth/react';
+import { TrainingLogDocument } from '../../../../server/mongodb/models/TrainingLog';
 
 export default function TrainingLogsPage() {
+    const { data: session, status } = useSession();
+    const [isLoading, setIsLoading] = useState(true);
+    const [logs, setLogs] = useState<TrainingLogDocument[]>([]);
 
-    const {data: session, status } = useSession();
-    const [logs, setLogs] = useState([]);
-
-    console.log('Current session ', session)
+    console.log('Current session ', session);
     useEffect(() => {
-      const fetchTrainingLogs = async () => {
-        await fetch('/api/training-log', {method: 'GET', credentials: 'include'}).then((response) => {
-          console.log(response);
-        })
-      }
-      if (session) {
-        fetchTrainingLogs();
-      }
-    }, [logs, session])
+        const fetchTrainingLogs = async () => {
+            try {
+                const response = await fetch('/api/training-log', {
+                    method: 'GET',
+                    credentials: 'include',
+                });
+                const respBody = await response.json();
+                const logs: TrainingLogDocument[] = respBody?.data;
 
+                if (logs != undefined) {
+                    setLogs(logs);
+                }
+                setIsLoading(false);
+            } catch (error) {
+                setIsLoading(false);
+            }
+        };
+
+        fetchTrainingLogs();
+    }, [isLoading]);
+
+    console.log('Training logs ', logs);
     return (
-            <main className="min-h-full w-full bg-white dark:bg-black flex flex-col">
-              <div className='p-8  flex flex-row justify-between items-center'>
+        <main className="min-h-full w-full bg-white dark:bg-black flex flex-col">
+            <div className="p-8  flex flex-row justify-between items-center">
                 <h1 className="text-left text-2xl font-bold text-neutral-600">
                     Training logs
                 </h1>
-                <Link className=' xl:mr-16' href="/dashboard/training-logs/new">
-                  <div className='flex flex-row gap-2'><Image width={24} height={24} src="/images/createNewLogo.png" alt="create new training log" /> <span className='font-bold text-neutral-400 '>Create New</span></div>
+                <Link className=" xl:mr-16" href="/dashboard/training-logs/new">
+                    <div className="flex flex-row gap-2">
+                        <Image
+                            width={24}
+                            height={24}
+                            src="/images/createNewLogo.png"
+                            alt="create new training log"
+                        />{' '}
+                        <span className="font-bold text-neutral-400 ">
+                            Create New
+                        </span>
+                    </div>
                 </Link>
-              </div>
-                
-                <hr />
-                <div className="mt-8 mx-4 overflow-y-scroll flex-1 flex flex-col gap-4 justify-start items-center">
-                    <TrainingLogCard data={mockData} />
-                    <TrainingLogCard data={mockData} />
-                    <TrainingLogCard data={mockData} />
-                </div>
-            </main>
+            </div>
+
+            <hr />
+            <div className="mt-8 mx-4 overflow-y-scroll flex-1 flex flex-col gap-4 justify-start items-center">
+                {logs.map((l, i) => (
+                    <TrainingLogCard
+                        key={i}
+                        data={{
+                            ...l,
+                            _id: l._id.toString(),
+                            user: l.user as any,
+                            animal: l.animal as any,
+                            date: l.date as any,
+                        }}
+                    />
+                ))}
+            </div>
+        </main>
     );
 }
